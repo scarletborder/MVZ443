3c8512e3f4bab73faf0cd428ba8ee29f705178c2
border scarlet <baishuibeef@gmail.com>
2025-03-12 09:25:50 +0800

    Updates

--- constants.ts
+++ constants.ts
@@ -10,7 +10,14 @@ export const updateContent = `
 `;
 
 export const announcement =
-    '游戏仍在开发中, 当前状态不代表最终品质'
+    `游戏仍在开发中, 当前状态不代表最终品质.
+    
+Pickaxe请按下Q键切换移除器械
+
+通过过关获得更多器械,目前进度以至chapter1-stage2,剧情仍然在完善中`
 
 
 export const SECKILL = 99889988;
+
+// 护盾器械,铲除上方时铲除,可以和任何非护盾器械兼容
+export const SHIELD_PLANT: number[] = [];
